ba822e7ff2a67137c17832edd8b034afb80d73b3
Katlego Mathebula <mathebulakatlego300@gmail.com>
2026-09-13 17:14:15 +0200

    Add programme model validation

--- backend/Models/Programme.cs
+++ backend/Models/Programme.cs
@@ -1,11 +1,16 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace Zenith.Api.Models;
 
 public class Programme
 {
     public int Id { get; set; }
 
+    [Required]
+    [StringLength(150)]
     public string Name { get; set; } = string.Empty;
 
+    [StringLength(1000)]
     public string? Description { get; set; }
 
     public bool IsActive { get; set; } = true;

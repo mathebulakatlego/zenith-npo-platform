50d4cb8745b27012d0fd2b933076849966438dfa
Katlego Mathebula <mathebulakatlego300@gmail.com>
2026-09-13 15:41:28 +0200

    Add applicant API

---
+++ backend/Controllers/ApplicantsController.cs
@@ -0,0 +1,49 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Zenith.Api.Data;
+using Zenith.Api.Models;
+
+namespace Zenith.Api.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class ApplicantsController : ControllerBase
+{
+    private readonly ZenithDbContext _context;
+
+    public ApplicantsController(ZenithDbContext context)
+    {
+        _context = context;
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<Applicant>>> GetApplicants()
+    {
+        return await _context.Applicants.ToListAsync();
+    }
+
+    [HttpGet("{id}")]
+    public async Task<ActionResult<Applicant>> GetApplicant(int id)
+    {
+        var applicant = await _context.Applicants.FindAsync(id);
+
+        if (applicant == null)
+        {
+            return NotFound();
+        }
+
+        return applicant;
+    }
+
+    [HttpPost]
+    public async Task<ActionResult<Applicant>> CreateApplicant(Applicant applicant)
+    {
+        _context.Applicants.Add(applicant);
+        await _context.SaveChangesAsync();
+
+        return CreatedAtAction(
+            nameof(GetApplicant),
+            new { id = applicant.Id },
+            applicant);
+    }
+}

a5207bfd5c479c52a035018deba27caa0d1f2907
Katlego Mathebula <mathebulakatlego300@gmail.com>
2026-09-13 15:40:15 +0200

    Add database context

---
+++ backend/Data/ZenithDbContext.cs
@@ -0,0 +1,25 @@
+using Microsoft.EntityFrameworkCore;
+using Zenith.Api.Models;
+
+namespace Zenith.Api.Data;
+
+public class ZenithDbContext : DbContext
+{
+    public ZenithDbContext(DbContextOptions<ZenithDbContext> options)
+        : base(options)
+    {
+    }
+
+    public DbSet<Programme> Programmes => Set<Programme>();
+    public DbSet<Applicant> Applicants => Set<Applicant>();
+    public DbSet<Application> Applications => Set<Application>();
+
+    public DbSet<Learner> Learners => Set<Learner>();
+
+    public DbSet<Cohort> Cohorts => Set<Cohort>();
+
+    public DbSet<Module> Modules => Set<Module>();
+
+    public DbSet<Progress> Progress => Set<Progress>();
+    public DbSet<Result> Results => Set<Result>();
+}

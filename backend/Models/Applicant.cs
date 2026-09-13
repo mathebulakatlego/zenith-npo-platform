b801f2de16e691049d1109361705f39e03be5bb0
Katlego Mathebula <mathebulakatlego300@gmail.com>
2026-09-13 17:17:45 +0200

    Add applicant model validation

--- backend/Models/Applicant.cs
+++ backend/Models/Applicant.cs
@@ -1,14 +1,25 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace Zenith.Api.Models;
 
 public class Applicant
 {
     public int Id { get; set; }
 
+    [Required]
+    [StringLength(100)]
     public string FirstName { get; set; } = string.Empty;
 
+    [Required]
+    [StringLength(100)]
     public string LastName { get; set; } = string.Empty;
 
+    [Required]
+    [EmailAddress]
+    [StringLength(255)]
     public string Email { get; set; } = string.Empty;
 
+    [Required]
+    [StringLength(20)]
     public string PhoneNumber { get; set; } = string.Empty;
 }

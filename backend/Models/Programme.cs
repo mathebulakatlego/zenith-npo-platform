561b38035dda84d90ca8bc7241eccae754a03054
Katlego Mathebula <mathebulakatlego300@gmail.com>
2026-09-13 15:38:23 +0200

    Add programme model

---
+++ backend/Models/Programme.cs
@@ -0,0 +1,12 @@
+namespace Zenith.Api.Models;
+
+public class Programme
+{
+    public int Id { get; set; }
+
+    public string Name { get; set; } = string.Empty;
+
+    public string? Description { get; set; }
+
+    public bool IsActive { get; set; } = true;
+}
